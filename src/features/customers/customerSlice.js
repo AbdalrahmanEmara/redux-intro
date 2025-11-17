41d3daa2ce00a73a6b64fe9ea50a6ed12040fedc
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-11-17 22:43:14 +0200

    Professional Redux file structure: State Slices

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,44 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: new Date().toISOString(),
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName: fullName,
+      nationalID: nationalID,
+    },
+  };
+}
+
+export function updateName(fullName) {
+  return {
+    type: "customer/updateCustomer",
+    payload: {
+      fullName: fullName,
+    },
+  };
+}
--- src/store.js
+++ src/store.js
@@ -1,65 +1,7 @@
 import { combineReducers, createStore } from "redux";
 
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-
-const initialStateCustomer = {
-  fullName: "",
-  nationalID: "",
-  createdAt: "",
-};
-
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan": {
-      if (state.loan > 0) return state;
-      // LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        balance: state.balance + action.payload.amount,
-        loanPurpose: action.payload.purpose,
-      };
-    }
-    case "account/payLoan": {
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    }
-    default:
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: new Date().toISOString(),
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-
-    default:
-      return state;
-  }
-}
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 const rootReducers = combineReducers({
   account: accountReducer,
@@ -68,55 +10,4 @@ const rootReducers = combineReducers({
 
 const store = createStore(rootReducers);
 
-function deposite(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount: amount, purpose: purpose },
-  };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName: fullName,
-      nationalID: nationalID,
-    },
-  };
-}
-
-function updateName(fullName) {
-  return {
-    type: "customer/updateCustomer",
-    payload: {
-      fullName: fullName,
-    },
-  };
-}
-
-store.dispatch(deposite(1000));
-console.log(store.getState());
-
-store.dispatch(withdraw(500));
-console.log(store.getState());
-
-store.dispatch(requestLoan(5000, "Buy a laptop"));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
-
-store.dispatch(createCustomer("Abdelrahman Emara", "33332212"));
-console.log(store.getState());
+export default store;
